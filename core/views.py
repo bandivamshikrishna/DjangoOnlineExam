2510b07903a9120bb850faf32675fbc5e49469b4
bandivamshikrishna <kvamshi340@gmail.com>
2022-01-26 23:16:49 +0530

    modified core/views.py file

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+def home(request):
+    return render(request,'core/home.html')
+
+
+def sign_in(request):
+    authform=AuthenticationForm()
+    return render(request,'core/signin.html',{'authform':authform})
