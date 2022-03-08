a5ca705650172a697b7fd2c9275dee0e93f3d4c4
ashok <boyaashok783@gmail.com>
2022-03-08 11:16:36 +0530

    adding admindashboard.html page

--- DjangoOnlineExam/urls.py
+++ DjangoOnlineExam/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('signin/',views.sign_in,name='signin'),
     path('signup/',views.sign_up,name='signup'),
     path('signin/afterlogin/',views.after_login,name='afterlogin'),
-    path('admin/dashboard/',views.admin_dashboard,name='admindashboard'),
+    path('admindashboard/',views.admin_dashboard,name='admindashboard'),
     path('student/',views.student_form,name='student'),
 ]
